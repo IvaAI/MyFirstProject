fc5e0fccfa1f5313f343b77cafd39fb4ac7ce210
Iva Ivanova <ospilak_bg@yahoo.com>
2017-06-18 17:52:21 +0300

    initial refactoring

--- File1.r
+++ File1.r
@@ -1,6 +1,10 @@
-data <- c(1,2,5)
-list <- l()
+customer1 <- 1
+customer2 <- 2
+customer3 <- 5
+
+customer.data <- c(customer1, customer2, customer3)
+
 count <- function(x){
-    res <- round(x+4, 3)
+    res <- round(x + 4, 3)
     return(res)
 }
